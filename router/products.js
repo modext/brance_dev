3a67faae881b715b13c29827876febcf64340435
modext <chigomorji@gmail.com>
2023-04-28 13:04:41 +0100

    update product.js

---
+++ router/products.js
@@ -0,0 +1,13 @@
+// import express from 'express';
+// import {
+//   deleteProducts,
+//   getAllProducts,
+//   postProducts,
+//   updateProducts,
+// } from '../controllers/productControllers.js';
+// const router = express.Router();
+
+// router.route('/').get(getAllProducts).post(postProducts);
+// router.route('/:id').patch(updateProducts).delete(deleteProducts);
+
+// export default router;
