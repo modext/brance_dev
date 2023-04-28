24b88b1010314983e93f64c8e9b550816ded05aa
modext <chigomorji@gmail.com>
2023-04-28 12:56:43 +0100

    production changes

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      className="mt-14 py-2 w-full text-white bg-[#FF385C]"
+      style={{
+        textAlign: "center",
+        fontSize: "1rem",
+        fontWeight: "bold"
+      }}
+    >
+      &copy; Designed and Developed by Danish Iqbal
+    </footer>
+  );
+}
+
+export default Footer;
