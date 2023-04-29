9fa90c3b58be8426fdb78bd61ac985e9232ab6f2
modext <chigomorji@gmail.com>
2023-04-29 09:47:14 +0100

    adding firebase storage for image uploading

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -3,7 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyB-dOKoHBLC1VEpJDNzt22raubJBfc2u9U",
+  authDomain: "brance-dev.firebaseapp.com",
+  projectId: "brance-dev",
+  storageBucket: "brance-dev.appspot.com",
+  messagingSenderId: "674533390838",
+  appId: "1:674533390838:web:bcb7accc26504276146fd7"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
