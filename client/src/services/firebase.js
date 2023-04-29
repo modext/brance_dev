51b566efb4af5429f24dbd75b5ca8c17f8c8dd7a
modext <chigomorji@gmail.com>
2023-04-29 09:46:32 +0100

    setting up firebase

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -3,14 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB-dOKoHBLC1VEpJDNzt22raubJBfc2u9U",
-  authDomain: "brance-dev.firebaseapp.com",
-  projectId: "brance-dev",
-  storageBucket: "brance-dev.appspot.com",
-  messagingSenderId: "674533390838",
-  appId: "1:674533390838:web:bcb7accc26504276146fd7"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -19,3 +12,4 @@ const auth = getAuth();
 const storage = getStorage();
 
 export { db, auth, storage };
+
