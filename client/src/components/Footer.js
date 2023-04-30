a762024f5f9122740eb2c6b6cc6cbf54c2cf3b6d
modext <chigomorji@gmail.com>
2023-04-30 05:04:42 +0100

    made minor changes to footer annd brands coponent

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer
-      className="mt-14 py-2 w-full text-white bg-[#FF385C]"
+      className="mt-14 py-2 w-full text-white bg-[#FF375C]"
       style={{
         textAlign: "center",
         fontSize: "1rem",
